c273706269059d3be744f7b5dabbebea4f3ad14a
zenith <zenithyap88@gmail.com>
2025-03-19 11:19:05 +0800

    Style navbar

--- src/home.js
+++ src/home.js
@@ -4,7 +4,7 @@ export default function renderHome() {
     const content = document.querySelector("#content");
 
     const title = createHtmlElement("h1", ["white", "title"], null, "Lorem Ipsum");
-    const subtitle = createHtmlElement("h3", ["white"], null, "Ad dolore anim incididunt voluptate tempor cupidatat aute reprehenderit anim");
+    const subtitle = createHtmlElement("p", ["white"], null, "Ad dolore anim incididunt voluptate tempor cupidatat aute reprehenderit anim");
 
     content.appendChild(title);
     content.appendChild(subtitle);
