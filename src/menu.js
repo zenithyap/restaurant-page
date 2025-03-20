1f8782ecb532c907f3add1d5d0ab7ded141c7eb5
zenith <zenithyap88@gmail.com>
2025-03-20 12:51:13 +0800

    Style menu

--- src/menu.js
+++ src/menu.js
@@ -7,20 +7,20 @@ export default function renderMenu() {
     const menu = createHtmlElement("div", null, "menu", null);
     const title = createHtmlElement("h1", ["dark", "title"], null, "Menu");
 
-    const gravida = createHtmlElement("div", [], "food-item", null);
-    const gravidaTitle = createHtmlElement("h3", ["dark"], null, "Gravida");
+    const gravida = createHtmlElement("div", ["food-item"], null, null);
+    const gravidaTitle = createHtmlElement("h2", ["gold"], null, "Gravida");
     const gravidaSubtitle = createHtmlElement("p", ["dark"], null, "Suspendisse nec felis auctor neque dignissim pulvinar et ac tortor.");
 
-    const felis = createHtmlElement("div", [], "food-item", null);
-    const felisTitle = createHtmlElement("h3", ["dark"], null, "Felis");
+    const felis = createHtmlElement("div", ["food-item"], null, null);
+    const felisTitle = createHtmlElement("h2", ["gold"], null, "Felis");
     const felisSubtitle = createHtmlElement("p", ["dark"], null, "Quisque faucibus metus vel velit accumsan, in pharetra massa ullamcorper.");
 
-    const pharetra = createHtmlElement("div", [], "food-item", null);
-    const pharetraTitle = createHtmlElement("h3", ["dark"], null, "Pharetra");
+    const pharetra = createHtmlElement("div", ["food-item"], null, null);
+    const pharetraTitle = createHtmlElement("h2", ["gold"], null, "Pharetra");
     const pharetraSubtitle = createHtmlElement("p", ["dark"], null, "Suspendisse maximus arcu nec urna scelerisque, idvarius mauris hendrerit.");
 
-    const commodo = createHtmlElement("div", [], "food-item", null);
-    const commodoTitle = createHtmlElement("h3", ["dark"], null, "Commodo");
+    const commodo = createHtmlElement("div", ["food-item"], null, null);
+    const commodoTitle = createHtmlElement("h2", ["gold"], null, "Commodo");
     const commodoSubtitle = createHtmlElement("p", ["dark"], null, "Nulla rhoncus lorem vel urna feugiat, at suscipit leovehicula.");
 
     menu.appendChild(title);
